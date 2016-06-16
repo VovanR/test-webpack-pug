d747ca522d2c22d1c02fdcaebc96ab2ddc95b0fe
Vladimir Rodkin <mail@vovanr.com>
2016-06-16 18:08:38 +0300

    Add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     entry: {
-        index: './src/index.pug'
+        index: './src/index.pug',
+        styles: './src/styles/styles.css'
     },
 	output: {
 		path: './dist',
@@ -11,6 +12,10 @@ module.exports = {
             {
                 test: /\.pug$/,
                 loader: 'file?name=[name].html!jade-html'
+            },
+            {
+                test: /\.css/,
+                loader: 'file?name=[name].css'
             }
         ]
     }
